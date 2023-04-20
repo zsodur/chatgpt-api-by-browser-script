3e78c044e544016febd1384dd5c4386b7a235cf7
zhangxiaobin <649924012@qq.com>
2023-04-20 15:55:34 +0800

    🐛 fix(tampermonkey-script.js): add try-catch block to handle errors when clicking on elements

--- tampermonkey-script.js
+++ tampermonkey-script.js
@@ -43,20 +43,24 @@ class App {
   async start({ text, model, newChat }) {
     this.stop = false
     if (newChat) {
-      const a = document.querySelector('a');
-      a.click();
-      await sleep(500);
-      const label = document.querySelector('label.block.text-xs.text-gray-700');
-      label.click();
-      await sleep(500);
-      if (model === 'gpt-4') {
-        const gpt4 = document.querySelector('li.group.relative.flex:last-child');
-        gpt4.click();
+      try {
+        const a = document.querySelector('a');
+        a.click();
         await sleep(500);
-      } else {
-        const gpt3 = document.querySelector('li.group.relative.flex:first-child');
-        gpt3.click();
+        const label = document.querySelector('label.block.text-xs.text-gray-700');
+        label.click();
         await sleep(500);
+        if (model === 'gpt-4') {
+          const gpt4 = document.querySelector('li.group.relative.flex:last-child');
+          gpt4.click();
+          await sleep(500);
+        } else {
+          const gpt3 = document.querySelector('li.group.relative.flex:first-child');
+          gpt3.click();
+          await sleep(500);
+        }
+      } catch (e) {
+        console.log(e);
       }
     }
     const textarea = document.querySelector('textarea');
